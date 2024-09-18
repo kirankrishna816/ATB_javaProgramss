cc6bd99e10d82390803634862c8583a37ac4b647
KIRAN <kirankrishna816@gmail.com>
2024-09-18 09:23:52 +0530

    hello

--- src/sept/ex_04092024/Lab002.java
+++ src/sept/ex_04092024/Lab002.java
@@ -2,3 +2,4 @@ package sept.ex_04092024;
 
 public class Lab002 {
 }
+ 
---
+++ src/sept/ex_18092024/MyFirstProgram.java
@@ -0,0 +1,7 @@
+package sept.ex_18092024;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
